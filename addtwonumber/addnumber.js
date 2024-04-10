1036423e847aa30d38dcf0141b11379fb472ae59
Sanjeev Yadav <78197691+Sanjeev2021@users.noreply.github.com>
2024-04-10 20:05:25 +0530

    Add files via upload

---
+++ addtwonumber/addnumber.js
@@ -0,0 +1,12 @@
+
+
+function addNumber(){
+  
+let num1 = document.querySelector("#num1").value
+let num2 = document.querySelector("#num2").value
+
+
+  const result = Number(num1) + Number(num2)
+  console.log(result)
+}
+
